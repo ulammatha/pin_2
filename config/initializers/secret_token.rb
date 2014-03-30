158487df80f65b209af13520792c823c1293b70b
ulammatha <udaykumar1117@gmail.com>
2014-03-30 15:13:40 -0700

    added bootstrap and devise gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pin2::Application.config.secret_key_base = '13c1a58a51fd3d79ab7d1bd09b944444ae3eb1478614a60e2363808a9fd5ff6272b3239b41e4e8da1730f5662f3998562763bd4e37aa15d9dc4b9ea781868472'
