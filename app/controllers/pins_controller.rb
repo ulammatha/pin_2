528d0dcbe12cee02e75ff31849501b7b664bd7d8
ulammatha <udaykumar1117@gmail.com>
2014-04-05 14:27:10 -0700

    Added pin scaffold and authenticated the users based on login

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except:[:index,:show]
+  before_action :correct_user, except:[:index,:show,:new,:create]
   # GET /pins
   # GET /pins.json
   def index
@@ -14,7 +15,7 @@ class PinsController < ApplicationController
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build#Pin.new
   end
 
   # GET /pins/1/edit
@@ -24,30 +25,41 @@ class PinsController < ApplicationController
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
+    respond_to do |format|
       if @pin.save
         format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @pin }
       else
         format.html { render action: 'new' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
       end
+    end
   end
 
   # PATCH/PUT /pins/1
   # PATCH/PUT /pins/1.json
   def update
+    respond_to do |format|
       if @pin.update(pin_params)
         format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
+        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
       end
+    end
   end
 
   # DELETE /pins/1
   # DELETE /pins/1.json
   def destroy
     @pin.destroy
+    respond_to do |format|
       format.html { redirect_to pins_url }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -55,7 +67,11 @@ class PinsController < ApplicationController
     def set_pin
       @pin = Pin.find(params[:id])
     end
-
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: 'Not Authorised to do the changes.' if @pin.nil?
+      #render action:'show', notice: 'Not Authorised to do the changes.' if @pin.nil?
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
