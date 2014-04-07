0927bb11166912bfc3c3918a973751a79c01f064
ulammatha <udaykumar1117@gmail.com>
2014-04-07 15:18:18 -0700

    Added user field in to table and validation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "app/assets/images/images.jpg"
 	 do_not_validate_attachment_file_type :image
+	 validates :image, presence: true
+	 validates :description, presence: true
 end
